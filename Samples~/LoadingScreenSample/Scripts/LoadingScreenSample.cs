ab53640ce58f75c94123e9f340d6d3cc51ea2129
NK Studio <NK-Studio@users.noreply.github.com>
2024-01-20 03:42:24 +0900

    String 방식 샘플 제공

--- Samples~/LoadingScreenSample/Scripts/LoadingScreenSample.cs
+++ Samples~/LoadingScreenSample/Scripts/LoadingScreenSample.cs
@@ -5,14 +5,23 @@ using UnityEngine.SceneSystem;
 public sealed class LoadingScreenSample : MonoBehaviour
 {
     public SceneLoader loadingScreenPrefab;
+
+#if USE_SCENE_REFERENCE
     public SceneReference sceneReference;
+#else
+    public string scenePath;
+#endif
+
 
     public void Load()
     {
         SceneLoader loadingScreen = Instantiate(loadingScreenPrefab);
         DontDestroyOnLoad(loadingScreen);
 
-        Scenes.LoadSceneAsync(sceneReference)
-            .WithLoadingScreen(loadingScreen);
+#if USE_SCENE_REFERENCE
+        Scenes.LoadSceneAsync(sceneReference).WithLoadingScreen(loadingScreen);
+#else
+        Scenes.LoadSceneAsync(scenePath).WithLoadingScreen(loadingScreen);
+#endif
     }
 }
